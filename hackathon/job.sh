504459ea1e5db623389af0d2ee1833cc71055182
Matt Sampson <ms0821@stellar-intel.princeton.edu>
2024-06-06 14:52:56 -0400

    adding output files

--- hackathon/job.sh
+++ hackathon/job.sh
@@ -4,6 +4,10 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --gres=gpu:1
+#SBATCH --reservation=openhack
+#SBATCH -o output.out
+#SBATCH -e output.err
+
 
 module load anaconda3/2023.3
 source /usr/licensed/anaconda3/2023.3/etc/profile.d/conda.sh
